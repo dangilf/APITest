fce22ec3ea865dd3862121bfb985c8f28ab16e56
dangilf <dangilf@mail.ru>
2018-05-23 01:03:02 +0300

    initial

---
+++ TestApp.DAL/Models/Project.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp.DAL.Models
+{
+    public class Project
+    {
+        public string Title { get; set; }
+        public string Key { get; set; }
+        public virtual List<Task> Tasks { get; set; }
+    }
+}
---
+++ TestApp.DAL/Models/Task.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp.DAL.Models
+{
+    public class Task: BaseEntity
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+        public virtual Employee Employee { get; set; }
+        public virtual Project Project { get; set; }
+
+
+    }
+}
